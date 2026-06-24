478cb2600b1616ddae564b70fea554d539f90cad
Szybet <53944559+Szybet@users.noreply.github.com>
2026-06-24 22:42:02 +0200

    fix?

--- quill-data-provider-lib/src/lib.rs
+++ quill-data-provider-lib/src/lib.rs
@@ -301,7 +301,8 @@ pub const WINDOW_SETTINGS_CONFIG_NAME: &str = "config.ron";
 
 pub fn load_window_settings(path: String) -> Vec<EinkWindowSetting> {
     if let Some(parent) = std::path::Path::new(&path).parent() {
-        std::fs::create_dir_all(parent)
+        // Not create_dir_all to avoid creating .config in encrypted home, when I mess things up
+        std::fs::create_dir(parent)
             .unwrap_or_else(|e| panic!("Failed to create directories for {}: {}", path, e));
     }
 
